dad2e2c1191a9b8dee160ab28f5c9a5e48894936
Midori <midori@takemotomidori-no-MacBook-Air-10.local>
2014-12-31 21:49:04 +0900

    Initial Commit

---
+++ electronic calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  electronic calculator
+//
+//  Created by Midori on 2014/12/31.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ electronic calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  electronic calculator
+//
+//  Created by Midori on 2014/12/31.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
